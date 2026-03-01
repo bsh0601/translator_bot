4bff0ec52e89b4d160fd52ade6fb44ea9c9c9b36
BSH <bsooh0601@gmail.com>
2026-03-01 19:35:42 +0900

    fix python version

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run_web():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
+threading.Thread(target=run_web).start()
+
 import discord
 from discord.ext import commands, tasks
 from googletrans import Translator
